6b3906a7068044b729578de0d420f405c53a063f
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2024-10-17 22:29:57 -0400

    csvgrep.rs: use rustfmt

--- src/csvgrep.rs
+++ src/csvgrep.rs
@@ -57,12 +57,10 @@ fn exactly_one<T>(values: &[T], predicate: fn(&T) -> bool) -> bool {
 fn main() -> Result<(), csv::Error> {
     let args: Args = argh::from_env();
 
-    if !exactly_one(
-        &[&args.regex, &args.fixed_string, &args.file_path],
-        |x| x.is_some()
-    ) {
-        eprintln!("csvgrep: error: Must specify exactly one of -r, -m, \
-                   and -f.");
+    if !exactly_one(&[&args.regex, &args.fixed_string, &args.file_path], |x| {
+        x.is_some()
+    }) {
+        eprintln!("csvgrep: error: Must specify exactly one of -r, -m, and -f.");
         std::process::exit(1);
     }
 
@@ -71,21 +69,21 @@ fn main() -> Result<(), csv::Error> {
     if let Some(regex_string) = args.regex {
         match Regex::new(&regex_string) {
             Err(error) => {
-                eprintln!("csvgrep: error: Cannot parse regular \
-                           expression: {error}");
+                eprintln!(
+                    "csvgrep: error: Cannot parse regular \
+                           expression: {error}"
+                );
                 std::process::exit(2);
-            },
+            }
             Ok(regex) => {
                 pattern = Pattern::Regex(regex);
-            },
+            }
         }
     } else if let Some(fixed_string) = args.fixed_string {
         pattern = Pattern::FixedString(fixed_string);
     } else if let Some(file_path) = args.file_path {
-        let Some(exact_strings) = load_file(&file_path)
-        else {
-            eprintln!("csvgrep: error: Cannot load contents of \
-                       '{file_path}'.");
+        let Some(exact_strings) = load_file(&file_path) else {
+            eprintln!("csvgrep: error: Cannot load contents of '{file_path}'.");
             std::process::exit(3);
         };
 
@@ -96,30 +94,32 @@ fn main() -> Result<(), csv::Error> {
 
     let mut csv_reader = csv_reader_from_stdin(args.delimiter)?;
 
-    let column_index: usize =
-        match csv_reader.header.iter().position(|value|
-                                                *value == args.column) {
-            Some(index) => index,
-            None => {
-                eprintln!("csvgrep: error: Column '{}' is not present \
-                           in the input.", args.column);
-                std::process::exit(4)
-            }
-        };
+    let column_index: usize = match csv_reader
+        .header
+        .iter()
+        .position(|value| *value == args.column)
+    {
+        Some(index) => index,
+        None => {
+            eprintln!(
+                "csvgrep: error: Column '{}' is not present in the input.",
+                args.column
+            );
+            std::process::exit(4)
+        }
+    };
 
-    let mut csv_writer =
-        csv_writer_to_stdout(Some(csv_reader.header.clone()))?;
+    let mut csv_writer = csv_writer_to_stdout(Some(csv_reader.header.clone()))?;
 
     for record in csv_reader.reader.records() {
         let input_record = record?;
         let cell = &input_record[column_index];
 
-        let record_matches: bool =
-            match &pattern {
-                Pattern::Regex(regex) => regex.find(cell).is_some(),
-                Pattern::FixedString(fixed_string) => cell.contains(fixed_string),
-                Pattern::File(exact_strings) => exact_strings.iter().any(|x| cell == x),
-            };
+        let record_matches: bool = match &pattern {
+            Pattern::Regex(regex) => regex.find(cell).is_some(),
+            Pattern::FixedString(fixed_string) => cell.contains(fixed_string),
+            Pattern::File(exact_strings) => exact_strings.iter().any(|x| cell == x),
+        };
 
         if args.invert ^ record_matches {
             csv_writer.writer.write_record(&input_record)?;
